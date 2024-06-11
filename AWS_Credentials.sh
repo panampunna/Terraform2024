3b2110ced11e1d24e8b4600999c1306271920f91
vjohn <perljohn@gmail.com>
2024-06-11 17:43:51 +0530

    AWS PW

--- AWS_Credentials.sh
+++ AWS_Credentials.sh
@@ -1,19 +1,28 @@
+#!/bin/bash
 
 
 echo "This is to export AWS IAM credentials for running the Terraform.   "
 
 echo -e "Please enter the AWS_ACCESS_KEY_ID = "
 read  ACCESS_KEY_ID
-echo "  $ACCESS_KEY_ID "
-export AWS_ACCESS_KEY_ID="$ACCESS_KEY_ID"
 
 echo -e "Please enter the AWS_SECRET_ACCESS_KEY = "
 read  SECRET_ACCESS_KEY
-echo  -e "  $SECRET_ACCESS_KEY " 
-export  AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
 
 echo -e "Please enter the AWS_REGION = "
 read  REGION
-echo -e "  $REGION " 
+
+echo -e "  $ACCESS_KEY_ID "
+echo -e "  $SECRET_ACCESS_KEY "
+echo -e "  $REGION "
+
+export AWS_ACCESS_KEY_ID="$ACCESS_KEY_ID"
+export  AWS_SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY"
 export AWS_REGION="$REGION"
+
+
+echo " If getting error copy and paste the below in Terminal"
+echo "export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID "
+echo "export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY"
+echo "export AWS_REGION=$REGION"
 
